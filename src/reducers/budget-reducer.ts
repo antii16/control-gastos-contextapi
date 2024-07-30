c2347ee2c0bfe0344a4ac2e45f4bd9a45cdea0d3
Antonella <98277771+antii16@users.noreply.github.com>
2024-07-30 21:50:22 +0200

    Crear formulario de gastos, instalar calendario y validación de formulario

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,12 +1,16 @@
 export type BudgetAction = 
-{type: "add-budget", payload: {budget:number}}
+{type: "add-budget", payload: {budget:number}} |
+{type: "show-modal"} |
+{type: "close-modal"}
 
 export type BudgetState = {
-    budget:number
+    budget:number,
+    modal: boolean
 }
 
 export const initialState: BudgetState ={
-    budget:0
+    budget:0,
+    modal: false
 }
 
 export const budgetReducer = (
@@ -17,6 +21,19 @@ export const budgetReducer = (
     return{
         ...state,
         budget: action.payload.budget
+    }
+   } 
+   if(action.type === "show-modal") {
+    return{
+        ...state,
+        modal: true
+    }
+   } 
+
+   if(action.type === "close-modal") {
+    return{
+        ...state,
+        modal: false
     }
    } 
    return state
